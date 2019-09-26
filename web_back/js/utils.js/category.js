f72200d9b6e6bd311f221626b8f3fcb311fc196c
tsp0104 <tsp0104@126.com>
2019-09-26 09:12:37 +0800

    文章类别管理

---
+++ web_back/js/utils.js/category.js
@@ -0,0 +1,22 @@
+var category = {
+    //显示
+    show:function () {
+
+    },
+    //插入
+    add:function () {
+
+    },
+
+    //删除
+    del:function () {
+
+    },
+
+    //删除
+    edit:function () {
+
+    },
+
+
+}
