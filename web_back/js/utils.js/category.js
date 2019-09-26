b72a32219fd2c8e889450b2466d586ddd7f04be4
tsp0104 <tsp0104@126.com>
2019-09-26 11:37:30 +0800

    文章类别列表数据转字符串

--- web_back/js/utils.js/category.js
+++ web_back/js/utils.js/category.js
@@ -1,20 +1,29 @@
+/**
+ * 文章类别管理
+ */
 var category = {
     //显示
-    show:function () {
+    show: function (callback) {
+        $.get(
+            'http://localhost:8000/admin/category_search',
 
+            function (res) {
+                callback(res)
+            }
+        )
     },
     //插入
-    add:function () {
+    add: function () {
 
     },
 
     //删除
-    del:function () {
+    del: function () {
 
     },
 
     //删除
-    edit:function () {
+    edit: function () {
 
     },
 
